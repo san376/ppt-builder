2f8e5822b4f1bccd2c59a51630dd421944a6e8dc
san376 <155067913+san376@users.noreply.github.com>
2025-03-27 02:37:23 +0530

    create

--- src/store/useSlideStore.tsx
+++ src/store/useSlideStore.tsx
@@ -1,8 +1,9 @@
-import { Slide, Theme } from '@/lib/types'
+import { ContentItem, Slide, Theme } from '@/lib/types'
 import { Project } from '@prisma/client'
 import {create} from 'zustand'
 import {persist} from 'zustand/middleware'
 import {v4 as uuidv4} from 'uuid'
+import { Content } from 'vaul'
 
 interface SlideState {
     slides: Slide[]
@@ -16,6 +17,12 @@ interface SlideState {
     getOrderedSlides: ()=> Slide[]
     reorderSlides: (fromIndex: number, toIndex: number) => void
     addSlideAtIndex: (slide:Slide, index: number)=> void
+    setCurrentSlide: (index: number) => void
+    updateContentItem: (
+        slideId: string,
+        contentId: string,
+        newContent: string | string[] | string[][]
+    )=> void
 }
 
 const defaultTheme: Theme = {
@@ -54,6 +61,40 @@ export const useSlidesStore = create(persist<SlideState>((set,get)=>
         removeSlide: (id)=> set((state)=> ({
             slides: state.slides.filter((slide)=> slide.id !== id),
         })),
+        updateContentItem: (slideId, contentId, newContent)=>{
+            set((state) => {
+                const updateContentRecursively = (item: ContentItem): ContentItem => {
+                  if (item.id === contentId) {
+                    return { ...item, content: newContent };
+                  }
+                  if (
+                    Array.isArray(item.content) &&
+                    item.content.every((i) => typeof i !== 'string')
+                  ) {
+                    return {
+                      ...item,
+                      content: item.content.map((subItem) => {
+                        if (typeof subItem !== 'string') {
+                          return updateContentRecursively(subItem as ContentItem);
+                        }
+                        return subItem
+                      }) as ContentItem[],
+                    }
+                  }
+                  return item
+                }
+                return{
+                    slides: state.slides.map((slide)=>
+                        slide.id === slideId
+                        ? { ...slide, Content: updateContentRecursively
+                            (slide.content)
+                        }: slide
+                    ),
+                }
+              });
+              
+        },
+        setCurrentSlide: (index)=> set({currentSlide: index}),
         reorderSlides: (fromIndex: number, toIndex: number) =>{
             set((state)=>{
                 const newSlides = [...state.slides]
